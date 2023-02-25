7a5100ce559e8ef51522b928b93814f686a9c29a
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2023-02-26 01:30:31 +0800

    Upload the main python script file.

---
+++ RelinkLatestVersion.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+dirList = next(os.walk(".\\", topdown=True, followlinks=False))[1]
+try:
+    dirList.remove('latest')
+except ValueError:
+    pass
+print('Select a directory to link: ')
+print('--------------')
+numTemp = 0
+for numDisp in dirList:
+    numTemp += 1
+    print(str(numTemp) + ": " + str(numDisp))
+print('--------------')
+while True:
+    try:
+        verIndex = int(input('Input the number: '))
+    except ValueError:
+        print('\033[0;33m[Warning]\033[0m Please enter a number. ')
+    else:
+        break
+subprocess.call('rd latest>nul', shell=True)
+subprocess.call(['mklink', '/J', 'latest', dirList[int(int(verIndex)-1)], '>nul'], shell=True)
+print('Done!')
+print('Press any key to quit.')
+os.system('pause>nul')
